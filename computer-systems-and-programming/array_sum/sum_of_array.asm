c1f207f29d5157ed46b6dc2b00ee334affe54324
nurlan4 <mr.tashpolotov@gmail.com>
2024-03-25 21:38:01 -0500

    Sum of array in assembly

---
+++ computer-systems-and-programming/array_sum/sum_of_array.asm
@@ -0,0 +1,25 @@
+global  _main
+extern  _printf
+
+section .data
+    array dd 1, 2, 3
+    arraySize dd 3
+    resultMessage db 'The result is: %d', 10, 0 
+section .text
+_main:
+    xor ebx,ebx
+    xor eax,eax
+    mov ecx, array
+    mov edx, [arraySize]
+loop1:
+    add eax, [ecx + ebx*4]
+    add ebx,1
+    cmp ebx, edx     
+    jl loop1
+
+    push eax        
+    push resultMessage 
+    call _printf    
+    add  esp, 8     
+
+    ret
