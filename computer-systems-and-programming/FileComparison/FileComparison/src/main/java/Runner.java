150044b8e99ac4963d9968ab70abc147bd377375
nurlan4 <tashpolotovnurlan@mail.ru>
2024-04-19 22:42:43 -0500

    File writing for project

--- computer-systems-and-programming/FileComparison/FileComparison/src/main/java/Runner.java
+++ computer-systems-and-programming/FileComparison/FileComparison/src/main/java/Runner.java
@@ -1,14 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Runner {
     public static String readFromInputStream(InputStream inputStream)
             throws IOException {
         StringBuilder resultStringBuilder = new StringBuilder();
-        try (BufferedReader br
-                     = new BufferedReader(new InputStreamReader(inputStream))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             String line;
             while ((line = br.readLine()) != null) {
                 resultStringBuilder.append(line).append("\n");
@@ -17,12 +15,62 @@ public class Runner {
         return resultStringBuilder.toString();
     }
     public static void main(String[] args) throws IOException {
-            String expectedData = "Hello, world!";
-
+            String expectedData = "Hello, world!!!!?";
             Class clazz = Runner.class;
             InputStream inputStream = clazz.getResourceAsStream("fileTest.txt");
             String data = readFromInputStream(inputStream);
-
             System.out.println(data);
+            int counter=300;
+            HashMap<Integer, String> encoding = new HashMap<>();
+            int l=0;
+            int r;
+            StringBuilder result= new StringBuilder();
+            while(l<data.length()-1){
+                if(Character.isLetter(data.charAt(l))){
+                    r=l+1;
+                    while(Character.isLetter(data.charAt(r))){
+                        r++;
+                    }
+                    result.append(data.substring(l,r));
+                    result.append(" ");
+                    encoding.put(counter,data.substring(l,r));
+                    counter++;
+                    l=r-1;
+                }else{
+                    do{
+
+                    result.append((int)data.charAt(l));
+                    result.append(" ");
+                    l++;}
+                    while ( l < data.length()-1 && !Character.isLetter( data.charAt(l)));
+                    l--;
+                }
+                l++;
+            }
+        System.out.println(result);
+
+
+        try {
+            File myObj = new File("output.sc");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        try {
+            FileWriter myWriter = new FileWriter("output.sc");
+            for (Map.Entry<Integer, String> j: encoding.entrySet()){
+                myWriter.write(j.getKey()+" = "+ j.getValue()+"\n"); }
+
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
